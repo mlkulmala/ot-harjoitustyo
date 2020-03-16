79b5d352f0d1eff244524d058f960d2d7ad9a767
Mona Kulmala <mlkulmala@hotmail.com>
2020-03-16 11:17:24 +0200

    pom

---
+++ laskarit/viikko2/Maksukortti/src/test/java/MaksukorttiTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mlkul
+ */
+public class MaksukorttiTest {
+    
+    public MaksukorttiTest() {
+    }
+    
+//    @BeforeClass
+//    public static void setUpClass() {
+//    }
+//    
+//    @AfterClass
+//    public static void tearDownClass() {
+//    }
+//    
+//    @Before
+//    public void setUp() {
+//    }
+//    
+//    @After
+//    public void tearDown() {
+//    }
+
+    
+    @Test
+    public void hello() {}
+    
+//    @Test
+//    public void konstruktoriAsettaaSaldonOikein() {
+//        Maksukortti kortti = new Maksukortti(10);
+//
+//        String vastaus = kortti.toString();
+//
+//        assertEquals("Kortilla on rahaa 10.0 euroa", vastaus);
+//    }
+}
